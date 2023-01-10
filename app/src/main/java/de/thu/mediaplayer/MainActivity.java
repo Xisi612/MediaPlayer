37396c3d4919ebc9a52278cf8a1832d58e945973
Xisi612 <isabellbaldamus@t-online.de>
2023-01-10 22:47:40 +0100

    Initial commit

---
+++ app/src/main/java/de/thu/mediaplayer/MainActivity.java
@@ -0,0 +1,40 @@
+package de.thu.mediaplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        logMusikList();
+    }
+
+    private void logMusikList(){
+        ContentResolver contentResolver = getContentResolver();
+        Uri musicUri = MediaStore.Audio.Media.INTERNAL_CONTENT_URI;
+        Cursor musicCursor = contentResolver.query(musicUri, null, null, null, null);
+
+        int titleColumn = musicCursor.getColumnIndex(MediaStore.Audio.Media.TITLE);
+        int artistColumn = musicCursor.getColumnIndex(MediaStore.Audio.Media.ARTIST);
+        int albumColumn = musicCursor.getColumnIndex(MediaStore.Audio.Media.ALBUM);
+        int durationColumn = musicCursor.getColumnIndex(MediaStore.Audio.Media.DURATION);
+
+        while (musicCursor.moveToNext()){
+            String title = musicCursor.getString(titleColumn);
+            String artist = musicCursor.getString(artistColumn);
+            String album = musicCursor.getString(albumColumn);
+            String duration = musicCursor.getString(durationColumn);
+            Log.d("MusicRetriever", "Title: " + title + " | Artist: " + artist + " | Album: " + album + " | Duration: " + duration);
+        }
+    }
+}
